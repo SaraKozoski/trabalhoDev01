efaa1eeb158791df020a749d767f00ea8dbc24bf
SaraKozoski <sarakozoski@gmail.com>
2024-07-24 10:29:28 -0300

    Criação componet response

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import forms from './components/forms.vue';
+import response from './components/response.vue';
 </script>
 
 <template>
-<forms />
-
+    <hr>
+    <div>
+        <forms/>
+    </div>
+    <hr>
+    <div>
+        <response/>
+    </div>
+    <hr>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
