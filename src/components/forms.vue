f5265dffab720a7cc0b560d3b4c417ef93cdf777
SaraKozoski <sarakozoski@gmail.com>
2024-07-25 09:32:26 -0300

    Conserto erros na pagina forms.vue e criação function salvar

--- src/components/forms.vue
+++ src/components/forms.vue
@@ -1,44 +1,62 @@
 <script setup>
-const nome = 'Formulário Pessoal'
+import { reactive } from 'vue';
+const titulo = 'Formulário Pessoal';
+const emit = defineEmits(['adicionar']);
+const cadastro = reactive({
+  nome: '',
+  email: '',
+  senha: '',
+  confirmacaoSenha: '',
+  dataNasc: '',
+  endereco: '',
+  cidade: ''
+})
+function salvar() {
+  if (cadastro.nome === '' || cadastro.senha === '') {
+    alert('Os campos nome e preço são obrigatórios');
+    return;
+  }
+  emit('adicionar', { ...cadastro });
+}
 </script>
 
 <template>
   <div class="title">
-    <h1>{{ nome }}</h1>
+    <h1>{{ titulo }}</h1>
   </div>
   <form id="form">
     <div class="container">
       <label for="name">Nome</label>
-      <input type="text" placeholder="Digite seu nome" />
+      <input type="text" v-model="cadastro.nome" placeholder="Digite seu nome" />
     </div>
 
     <div class="container">
       <label for="email">E-mail</label>
-      <input type="text" placeholder="Digite seu E-mail" />
+      <input type="text" v-model="cadastro.email" placeholder="Digite seu E-mail" />
     </div>
     <div class="container">
       <label for="password">Senha</label>
-      <input type="text" placeholder="Digite sua Senha" />
+      <input type="text" v-model="cadastro.senha" placeholder="Digite sua Senha" />
     </div>
     <div class="container">
       <label for="password">Confirme sua Senha</label>
-      <input type="text" placeholder="Digite sua Senha" />
+      <input type="text" v-model="cadastro.confirmacaoSenha" placeholder="Digite sua Senha" />
     </div>
     <div class="container">
       <label for="date">Data de nascimento</label>
-      <input type="text" placeholder="Digite sua data de nascimento" />
+      <input type="text" v-model="cadastro.dataNasc" placeholder="Digite sua data de nascimento" />
     </div>
     <div class="container">
       <label for="address">Endereço</label>
-      <input type="text" placeholder="Digite seu endereço" />
+      <input type="text" v-model="cadastro.endereco" placeholder="Digite seu endereço" />
     </div>
     <div class="container">
       <label for="city">Cidade</label>
-      <input type="text" placeholder="Digite sua cidade" />
+      <input type="text" v-model="cadastro.cidade" placeholder="Digite sua cidade" />
     </div>
     <div class="container">
       <label for="ES">Selecione seu estado:</label>
-      <select name="ES" id="estado" v-model="estado">
+      <select name="ES" id="estado" v-model="cadastro.estado">
         <option value="">AC </option>
         <option value="">AL</option>
         <option value="">AP</option>
@@ -47,25 +65,25 @@ const nome = 'Formulário Pessoal'
         <option value="">CE</option>
         <option value="">DF</option>
         <option value="">ES</option>
-        <optin value="">GO</optin>
-        <optin value="">MA</optin>
-        <optin value="">MT</optin>
-        <optin value="">MS</optin>
-        <optin value="">MG</optin>
-        <optin value="">PA</optin>
-        <optin value="">PB</optin>
-        <optin value="">PR</optin>
-        <optin value="">PE</optin>
-        <optin value="">PI</optin>
-        <optin value="">RJ</optin>
-        <optin value="">RN</optin>
-        <optin value="">RS</optin>
-        <optin value="">RO</optin>
-        <optin value="">RR</optin>
-        <optin value="">SC</optin>
-        <optin value="">SP</optin>
-        <optin value="">SE</optin>
-        <optin value="">TO</optin>
+        <option value="">GO</option>
+        <option value="">MA</option>
+        <option value="">MT</option>
+        <option value="">MS</option>
+        <option value="">MG</option>
+        <option value="">PA</option>
+        <option value="">PB</option>
+        <option value="">PR</option>
+        <option value="">PE</option>
+        <option value="">PI</option>
+        <option value="">RJ</option>
+        <option value="">RN</option>
+        <option value="">RS</option>
+        <option value="">RO</option>
+        <option value="">RR</option>
+        <option value="">SC</option>
+        <option value="">SP</option>
+        <option value="">SE</option>
+        <option value="">TO</option>
       </select>
     </div>
   </form>
