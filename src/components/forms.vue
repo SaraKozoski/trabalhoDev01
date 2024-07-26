6ce63d2470f8f3ffae7d1a44f314b409d649f8d2
lunabolsoni <bolsoniluna@gmail.com>
2024-07-26 09:32:14 -0300

    formulario-dados

--- src/components/forms.vue
+++ src/components/forms.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 const titulo = 'Formulário Pessoal';
 const emit = defineEmits(['adicionar']);
 const cadastro = reactive({
@@ -18,6 +18,9 @@ function salvar() {
   }
   emit('adicionar', { ...cadastro });
 }
+const estado = ref([
+  "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR",
+   "PE", "PI", "RJ", "RN","RS","RO","RR","SC","SE","SC","TO"])
 </script>
 
 <template>
@@ -36,11 +39,11 @@ function salvar() {
     </div>
     <div class="container">
       <label for="password">Senha</label>
-      <input type="text" v-model="cadastro.senha" placeholder="Digite sua Senha" />
+      <input type="password" v-model="cadastro.senha" placeholder="Digite sua Senha" />
     </div>
     <div class="container">
       <label for="password">Confirme sua Senha</label>
-      <input type="text" v-model="cadastro.confirmacaoSenha" placeholder="Digite sua Senha" />
+      <input type="password" v-model="cadastro.confirmacaoSenha" placeholder="Digite sua Senha" />
     </div>
     <div class="container">
       <label for="date">Data de nascimento</label>
@@ -54,46 +57,75 @@ function salvar() {
       <label for="city">Cidade</label>
       <input type="text" v-model="cadastro.cidade" placeholder="Digite sua cidade" />
     </div>
+
     <div class="container">
-      <label for="ES">Selecione seu estado:</label>
-      <select name="ES" id="estado" v-model="cadastro.estado">
-        <option value="">AC </option>
-        <option value="">AL</option>
-        <option value="">AP</option>
-        <option value="">AM</option>
-        <option value="">BA</option>
-        <option value="">CE</option>
-        <option value="">DF</option>
-        <option value="">ES</option>
-        <option value="">GO</option>
-        <option value="">MA</option>
-        <option value="">MT</option>
-        <option value="">MS</option>
-        <option value="">MG</option>
-        <option value="">PA</option>
-        <option value="">PB</option>
-        <option value="">PR</option>
-        <option value="">PE</option>
-        <option value="">PI</option>
-        <option value="">RJ</option>
-        <option value="">RN</option>
-        <option value="">RS</option>
-        <option value="">RO</option>
-        <option value="">RR</option>
-        <option value="">SC</option>
-        <option value="">SP</option>
-        <option value="">SE</option>
-        <option value="">TO</option>
-      </select>
+      <label for="">Selecione seu estado:</label>
+      <div class="ES">
+        <select :id="type" v-model="perfil.estado">
+          <option disabled value="Es">Selecione um estado</option>
+          <option v-for="(estados, index) in estado" :key="index" :value="estado.sigla">
+            {{ estado.nome }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <div class="container">
+      <label for="language">Linguagem que voce usa:</label>
+      <input type="text" v-model="linguagem" placeholder="Digite a linguagem" />
+    </div>
+    <div class="container">
+      <label for="hobby">Hobbies do seu tempo livre:</label>
+      <input type="text" v-model="hobbies" placeholder="Digite seu hobbie " />
+    </div>
+    <div class="container">
+      <label for="biografia">Biografia:</label>
+      <input type="text" v-model="biografia" placeholder="Digite sua biografia " />
     </div>
     <button type="submit">Salvar</button>
+
   </form>
+
 </template>
 
 <style scoped>
 .title {
   margin: auto;
   text-align: center;
-  color: rgb(124, 124, 206);
+  color: rgb(9, 9, 148);
+}
+
+#form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+
+  margin-bottom: 20px;
+  gap: 20px;
+}
+
+.container input {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+}
+
+label {
+  font-weight: bold;
+  color: #222;
+}
+
+.submit {
+  font-weight: bold;
+  border: 1px solid #222;
+  font-size: 10px;
+  padding: 5px;
+  margin: 0 auto;
 }
 </style>
